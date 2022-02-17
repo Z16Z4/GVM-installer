ddfc2d0dc435459c5314582cfacc5829c2c8efd4
Z16Z4 <78325649+Z16Z4@users.noreply.github.com>
2022-02-17 12:51:46 +0000

    Delete install-gvm-part2.py

--- install-gvm-part2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3 
-
-import os 
-
-os.system('''sudo sed -i 's/\"$/\:\/opt\/gvm\/bin\:\/opt\/gvm\/sbin\:\/opt\/gvm\/\.local\/bin\"/g' /etc/environment''')
-os.system('sudo echo "/opt/gvm/lib" > /etc/ld.so.conf.d/gvm.conf')
